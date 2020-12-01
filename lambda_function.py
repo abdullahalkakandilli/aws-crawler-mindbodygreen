6c5f36c3ed597a013b35bd7f0c0748c3d2b0a000
Alka Kandilli <58339271+abdullahalkakandilli@users.noreply.github.com>
2020-12-01 18:23:06 +0300

    Add files via upload

---
+++ lambda_function.py
@@ -0,0 +1,121 @@
+'''
+   
+Get inital datetime from cralwtext.txt - aws 3 --> last_execute
+
+Get last_feed datetime from mindbodygreen rss --> last_feed
+
+Compare last_execute & last_feed
+
+Crawl web indexes after last_execute until last_feed from .rss
+
+Write list to csv file
+
+Upload csv file to Aws s3 Bucket
+
+'''
+import urllib.request
+from bs4 import BeautifulSoup
+import feedparser
+import datetime
+import boto3
+import csv
+
+
+
+def lambda_handler(event, context):
+    print(__doc__)
+    s3 = boto3.client("s3")
+    s3v2 = boto3.resource('s3')
+    if event:
+        file_obj = event["Records"][0]
+        bucketname = str(file_obj['s3']['bucket']['name'])
+        
+        fileObj = s3.get_object(Bucket=bucketname, Key="crawltext.txt")
+        file_content = fileObj["Body"].read().decode('utf-8')
+   
+    
+        
+        last_execute = datetime.datetime.strptime(file_content, '%a, %d %b %Y %H:%M:%S %Z')
+        #last_execute will come from aws S3 config datatime
+        print("last execute log: ", last_execute)
+        print("last feed log: ")
+        feed = feedparser.parse('https://www.mindbodygreen.com/rss/feed.xml')
+        
+        last_feed = datetime.datetime.strptime(feed.entries[0].published, '%a, %d %b %Y %H:%M:%S %Z')
+        #websites last entry's publish time - last_feed
+        
+        
+        
+        
+        
+
+        if last_execute == last_feed:
+            print("all is update")   #if there is no new entry, exit script
+            
+        else:
+            title_list = []
+            link_list = []
+            
+            for entry in feed.entries:
+                pub_date = datetime.datetime.strptime(entry.published, '%a, %d %b %Y %H:%M:%S %Z')
+                print("pub date log: ", pub_date)
+                test_log = entry.link
+                if(pub_date > last_execute):
+                    title_list.append(entry.title)
+                    link_list.append(entry.link)
+                    print("entry link log: ",test_log )
+            
+        body_list = []
+        
+        for val in link_list:
+            full_para =""
+            soup2 = BeautifulSoup(urllib.request.urlopen(val).read(),"html.parser")
+            b_tags = soup2.find_all("p") #find all <p> items from html page resource
+            
+            for i in b_tags:
+                #collecting paragraphs
+                final_para = " ".join(full_para.split())
+                print(final_para)
+                
+            body_list.append(final_para)
+    #--------------------------------------------------------------------#        
+    feed = feedparser.parse('https://www.mindbodygreen.com/rss/feed.xml')        
+    key2 = 'crawltext.txt'
+    fileObj3 = s3.get_object(Bucket=bucketname, Key="crawltext.txt")
+    with open('/tmp/crawltext.txt', 'w') as f:
+        f.write(feed.entries[0].published)
+       
+    bucket = s3v2.Bucket(bucketname)
+    bucket.upload_file('/tmp/crawltext.txt',key2)
+    
+    #--------------------------------------------------------------------#
+    
+    key = 'mycrawrecord.csv'
+    getfile = s3.get_object(Bucket=bucketname, Key="mycrawrecord.csv")
+    #Only then you can write the data into the '/tmp' folder.
+    rows = zip(title_list,link_list,body_list)
+    with open('/tmp/mycrawrecord.csv', 'w') as f:
+        writer = csv.writer(f)
+        for row in rows:
+            writer.writerow(row)
+           
+    #upload the data into s3
+    bucket = s3v2.Bucket(bucketname)
+    bucket.upload_file('/tmp/mycrawrecord.csv', key)
+    
+    
+    
+    
+          
+    
+    
+   
+
+        
+
+    
+    
+  
+    
+    
+    
